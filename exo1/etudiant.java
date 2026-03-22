b426767f68d3a09b12a79dfbf2a20efc2d610d5f
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-03-22 15:57:03 +0100

    first commit and adding exo1

---
+++ exo1/etudiant.java
@@ -0,0 +1,55 @@
+
+
+import java.util.Objects;
+
+public class etudiant {
+    private int id;
+    private String nom;
+    private String prenom;
+    public etudiant(String nom,String prenom,int id){
+        this.id=id;
+        this.nom=nom;
+        this.prenom=prenom;
+    }
+    public String getnom(){
+        return nom;
+    }
+    public String getprenom(){
+        return prenom;
+    }
+    public int getid(){
+        return id;
+    }
+    public void setid(int id){
+        this.id=id;
+        
+    }
+    public void setnom(String nom){
+        this.nom=nom;
+    }
+    public void setprenom(String prenom){
+        this.prenom=prenom;
+    }
+    @Override
+    public String toString(){
+        return "nom: "+nom+" prenom: "+prenom+" id: "+id; 
+    }
+    @Override
+    public boolean equals(Object Et){
+        if (!(Et instanceof etudiant)) return false;
+        etudiant e=(etudiant) Et;
+
+        return  id==e.id;
+
+    }
+    @Override 
+    public int hashCode(){
+        return Objects.hash(id);
+
+    }
+    
+
+
+    
+
+}
---
+++ exo1/main.java
@@ -0,0 +1,95 @@
+
+
+import java.util.*;
+
+
+
+
+public class main {
+    public static void main(String[] args) {
+        
+        List<etudiant> etu=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number of etudiant");
+        int n=sc.nextInt();
+        sc.nextLine();
+        int id;
+        String name;
+        String sname;
+        
+        for (int i=0;i<n;i++){
+            System.out.println("enter id");
+            id=sc.nextInt();
+            sc.nextLine();
+            System.out.println("enter name");
+            name=sc.nextLine();
+            
+            System.out.println("enter second name");
+            sname=sc.nextLine();
+            
+            etudiant etud=new etudiant(name, sname, id);
+            etu.add(etud);
+
+        }
+
+
+        String text;
+        for(int i=0;i<n;i++){
+            text=etu.get(i).toString();
+            System.out.println(text);
+        
+        }
+
+        Set<etudiant> etuset=new HashSet<>(etu) ;
+
+        for(etudiant e:etuset){
+            System.out.println(e);
+        }
+
+
+        System.out.println("set size "+etuset.size());
+        System.out.println("list size "+etu.size());
+
+        etudiant e;
+        for(int i=0;i<etu.size();i++){
+            for(int j=i+1;j<etu.size();j++){
+                if(etu.get(i).getid()>etu.get(j).getid()){
+                    e=etu.get(i);
+                    etu.set(i, etu.get(j));
+                    etu.set(j, e);
+                }
+            }
+
+        }
+        
+        for(int i=0;i<etu.size();i++){
+            for(int j=i+1;j<etu.size();j++){
+                if(etu.get(i).getnom().compareTo(etu.get(j).getnom())>0){
+                    e=etu.get(i);
+                    etu.set(i, etu.get(j));
+                    etu.set(j, e);
+                }
+
+
+            }
+        }
+
+
+
+
+    
+    }
+
+    public etudiant search(List<etudiant> etu,etudiant eleve){
+        int n=etu.size();
+        for(int i=0;i<n;i++){
+            if (etu.get(i).getid()==eleve.getid()){
+                return etu.get(i);
+            }
+
+
+        }
+        return null;
+
+    }
+}
